ab6f80b709dfe97cfc91cac668c08b6b733e491b
Hendrik Geiger <g9a2hvk9@gmail.com>
2021-12-07 11:08:54 +0100

    runs build command and adds build pack

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount(){
+    this.getData()
+  }
+
+  getData(){
+    const conn = new XMLHttpRequest();
+    const url = "http://84.252.122.16:3000";
+    // conn.addEventListener('load', () => {
+    //   console.log(conn.responseText)
+    // })
+    conn.open("GET", url + "/rpc/proximity_search_inv?latitude=45.436699&longitude=12.320233&dist =3000")
+    conn.send()
+    conn.onreadystatechange = (e) => {
+      console.log(conn.responseText)
+    }
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          Hello World!
+        </header>
+        <div>
+        </div>
+      </div>
+    );
+
+  }
 }
 
 export default App;
