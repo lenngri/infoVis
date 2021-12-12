64c17bf35a740c87ffad522ce2b8746688a266a4
Grigoleit Lennart <lennart.grigoleit@tum.de>
2021-12-12 11:24:11 +0100

    Add test data, Colorpleth and Data import component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
-import './App.css';
 
 class App extends Component {
-
-  componentDidMount(){
-    this.getData()
-  }
-
-  getData(){
-    const conn = new XMLHttpRequest();
-    const url = "http://84.252.122.16:3000";
-    // conn.addEventListener('load', () => {
-    //   console.log(conn.responseText)
-    // })
-    conn.open("GET", url + "/rpc/proximity_search_inv?latitude=45.436699&longitude=12.320233&dist =3000")
-    conn.send()
-    conn.onreadystatechange = (e) => {
-      console.log(conn.responseText)
-    }
-  }
-
-
   render(){
     return (
       <div className="App">
---
+++ src/components/Colorpleth.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Colorpleth = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Colorpleth
