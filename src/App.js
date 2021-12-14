acbe7dc2bf20a4fb3feb12effa0501ab285d6141
Hendrik Geiger <g9a2hvk9@gmail.com>
2021-12-14 10:30:43 +0100

    changes app JS and pushes to heroku master

--- src/App.js
+++ src/App.js
@@ -3,30 +3,11 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-
-  componentDidMount(){
-    this.getData()
-  }
-
-  getData(){
-    const conn = new XMLHttpRequest();
-    const url = "http://84.252.122.16:3000";
-    // conn.addEventListener('load', () => {
-    //   console.log(conn.responseText)
-    // })
-    conn.open("GET", url + "/rpc/proximity_search_inv?latitude=45.436699&longitude=12.320233&dist=3000")
-    conn.send()
-    conn.onreadystatechange = (e) => {
-      console.log(conn.responseText)
-    }
-  }
-
-
   render(){
     return (
       <div className="App">
         <header className="App-header">
-          Hello World!
+          Hello World! This is team toppo (43)!
         </header>
         <div>
         </div>
