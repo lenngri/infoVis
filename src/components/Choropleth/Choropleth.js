34de5183100141ba094b6c4dd5ea714d40b00ba7
Grigoleit Lennart <lennart.grigoleit@tum.de>
2021-12-14 18:54:52 +0100

    First working colorpleth

--- src/components/Choropleth/Choropleth.js
+++ src/components/Choropleth/Choropleth.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import { useMapData } from './useMapData';
+import { usePatentData } from '../../datatools/usePatentData';
 import { Marks } from './Marks';
+import { scaleSequential, max, interpolatePuBu } from 'd3';
 
 const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+const selectedYear = 2008;
 
 const Colorpleth = () => {
     const mapData = useMapData();
-    console.log(mapData)
+    const patentData = usePatentData();
     
-    if (!mapData) {
+    if (!mapData || !patentData) {
         return <p>Loading...</p>;
     }
+    const filteredData = patentData.filter(d => d.c0 === selectedYear);
+    const colorValue = d => d.c2;
+
+    const rowByCountry = new Map();
+    filteredData.forEach(d => {
+        rowByCountry.set(d.c1, d)
+    })
+
+    const colorScale = scaleSequential(interpolatePuBu)
+        .domain([0, 20000]) //max(filteredData, colorValue
 
     return (
         <svg width={width} height={height}>
-            <Marks data={mapData} width={width} height={height} />
+            <Marks 
+                mapData={mapData} 
+                width={width}
+                height={height}
+                rowByCountry={rowByCountry}
+                colorScale={colorScale}
+                colorValue={colorValue}
+            />
         </svg>
     );
 }
--- src/components/Choropleth/Marks.js
+++ src/components/Choropleth/Marks.js
@@ -1,17 +1,34 @@
 import { geoPath, geoMercator } from 'd3';
 
+const missingDataColor = 'white';
 
-
-export const Marks = ({data, width, height}) => {
+export const Marks = ({
+  mapData,
+  width,
+  height,
+  rowByCountry,
+  colorScale,
+  colorValue }) => {
 
   const projection = geoMercator().scale(500).translate([width / 2, height]);
   const path = geoPath(projection);
 
   return (
     <g className="marks">
-      {data.features.map(feature => (
-        <path className='land' d={path(feature)}/>
-      ))}
+      {mapData.features.map(feature => {
+        const d = rowByCountry.get(feature.properties.geounit)
+
+        if(!d){
+          console.log("Name doesn't match: " + feature.properties.geounit)
+        }
+
+        return <path
+          fill={d ? colorScale(colorValue(d)) : missingDataColor} 
+          d={path(feature)}
+        />
+      })}
     </g>
   );
 };
+
+// fill={colorScale(colorValue(d))}
