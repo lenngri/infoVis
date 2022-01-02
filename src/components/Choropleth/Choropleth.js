8930e03823117ae78561dcab7363fcb6d59b3c92
Grigoleit Lennart <lennart.grigoleit@tum.de>
2022-01-02 14:00:36 +0100

    First working legend

--- src/components/Choropleth/Choropleth.js
+++ src/components/Choropleth/Choropleth.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useMapData } from './useMapData';
 import { usePatentData } from '../../datatools/usePatentData';
-import { Marks } from './Marks';
-import { scaleThreshold, max, interpolatePuBu } from 'd3'; // scaleSequential
+import Marks from './Marks';
+import { scaleThreshold } from 'd3'; // scaleSequential
+import { schemeBlues } from 'd3-scale-chromatic';
 
 const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 const selectedYear = 2008;
+const legendTitle = 'Number of Patents registered per Year';
 
 const Colorpleth = () => {
   const mapData = useMapData();
   const patentData = usePatentData();
+  const svg = useRef(null);
 
   if (!mapData || !patentData) {
     return <p>Loading...</p>;
@@ -25,19 +28,22 @@ const Colorpleth = () => {
 
   const colorScale = scaleThreshold()
     .domain([100, 1000, 5000, 10000, 50000, 100000])
-    .range(['#eff3ff', '#c6dbef', '#9ecae1', '#6baed6', '#3182bd', '#08519c']);
+    .range(schemeBlues[6]);
 
   return (
-    <svg width={width} height={height}>
-      <Marks
-        mapData={mapData}
-        width={width}
-        height={height}
-        rowByCountry={rowByCountry}
-        colorScale={colorScale}
-        colorValue={colorValue}
-      />
-    </svg>
+    <>
+      <svg width={width} height={height} ref={svg}>
+        <Marks
+          mapData={mapData}
+          width={width}
+          height={height}
+          rowByCountry={rowByCountry}
+          colorScale={colorScale}
+          colorValue={colorValue}
+          legendTitle={legendTitle}
+        />
+      </svg>
+    </>
   );
 };
 
--- src/components/Choropleth/Marks.js
+++ src/components/Choropleth/Marks.js
@@ -1,32 +1,47 @@
+import { useEffect, useRef } from 'react';
 import { geoPath, geoMercator } from 'd3';
+import Legend from '../../charttools/useLegend';
 
 const missingDataColor = 'darkgray';
 
-export const Marks = ({ mapData, width, height, rowByCountry, colorScale, colorValue }) => {
+const Marks = ({ mapData, width, height, rowByCountry, colorScale, colorValue, legendTitle }) => {
+  // generate map progjection and paths
   const projection = geoMercator()
     .scale(500)
     .translate([width / 2, height]);
   const path = geoPath(projection);
 
+  // generate map legend and append to svg
+  const group = useRef(null);
+  const legend = Legend(colorScale, { title: legendTitle });
+  useEffect(() => {
+    if (group.current) {
+      group.current.appendChild(legend);
+    }
+  }, [legend, group]);
+
   return (
-    <g className="marks">
-      {mapData.features.map((feature) => {
-        const d = rowByCountry.get(feature.properties.geounit);
+    <>
+      <g className="marks">
+        {mapData.features.map((feature) => {
+          const d = rowByCountry.get(feature.properties.geounit);
 
-        if (!d) {
-          console.log("Name doesn't match: " + feature.properties.geounit);
-        }
+          if (!d) {
+            console.log("Name doesn't match: " + feature.properties.geounit);
+          }
 
-        return (
-          <path
-            fill={d ? colorScale(colorValue(d)) : missingDataColor}
-            className="land"
-            d={path(feature)}
-          />
-        );
-      })}
-    </g>
+          return (
+            <path
+              fill={d ? colorScale(colorValue(d)) : missingDataColor}
+              className="land"
+              d={path(feature)}
+            />
+          );
+        })}
+      </g>
+      <g ref={group}></g>
+    </>
   );
 };
 
-// fill={colorScale(colorValue(d))}
+export default Marks;
