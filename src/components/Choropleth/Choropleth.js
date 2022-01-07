bf9db382609c80d0ec559a43a1565df86abc698f
Grigoleit Lennart <lennart.grigoleit@tum.de>
2022-01-07 11:17:30 +0100

    Filter population data by year and append to patent data

--- src/components/Choropleth/Choropleth.js
+++ src/components/Choropleth/Choropleth.js
@@ -1,3 +1,6 @@
+// Based on
+// Source: Curran Kelleher, 2018 https://www.youtube.com/watch?v=OoZ0LWD9KUs
+// Source: https://github.com/viswesh/Maps/tree/master/chapter1
 import React, { useState } from 'react';
 import { useMapData } from './useMapData';
 import { usePatentData } from '../../datatools/usePatentData';
@@ -22,14 +25,34 @@ const Colorpleth = () => {
     return <p>Loading...</p>;
   }
 
-  const filteredData = patentData.filter((d) => d.c0 === selectedYear);
-  const colorValue = (d) => d.c2;
+  // filter Data Sets for selected year
+  const filteredPatentData = patentData.filter((d) => d.year === selectedYear);
+  const filteredPopulationData = populationData.filter((d) => d.year === selectedYear);
 
-  const rowByCountry = new Map();
-  filteredData.forEach((d) => {
-    rowByCountry.set(d.c1, d);
+  // create mapping between country name and population size
+  let populationById = {};
+  filteredPopulationData.forEach((d) => {
+    populationById[d.country] = {
+      year: +d.year,
+      population: +d.population,
+    };
+  });
+  // append population size to patent data set
+  filteredPatentData.forEach((d) => {
+    d.populationSize = populationById[d.country] ? populationById[d.country].population : {}; // geounit
   });
 
+  console.log(filteredPatentData);
+
+  // create mapping table between country name and normed patent data
+  const rowByCountry = new Map();
+  filteredPatentData.forEach((d) => {
+    rowByCountry.set(d.country, d);
+  });
+  console.log(rowByCountry);
+
+  // set colorValue function and colorScale object
+  const colorValue = (d) => d.patents;
   const colorScale = scaleThreshold()
     .domain([100, 1000, 5000, 10000, 50000, 100000])
     .range(schemeBlues[7]);
