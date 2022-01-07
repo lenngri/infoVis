a052b5bd0de9b961ef24e2a95181cc20f8d6499a
Grigoleit Lennart <lennart.grigoleit@tum.de>
2022-01-07 16:11:43 +0100

    align use...Data.js files

--- src/datatools/usePatentData.js
+++ src/datatools/usePatentData.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react';
 import { json } from 'd3';
 
-const jsonUrl = 'data/patents_by_year_and_country_name.json';
+const dataUrl = 'data/patents_by_year_and_country_name.json';
 
 export const usePatentData = () => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    json(jsonUrl).then((data) => {
+    json(dataUrl).then((data) => {
       setData(data);
     });
   }, []);
--- src/datatools/usePopulationData.js
+++ src/datatools/usePopulationData.js
@@ -1,18 +1,16 @@
 import { useState, useEffect } from 'react';
 import { json } from 'd3';
 
-const populationDataUrl = 'data/population_by_year_and_country_name.json';
+const dataUrl = 'data/population_europ.json';
 
 export const usePopulationData = () => {
-  const [populationData, setPopulationData] = useState(null);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-    json(populationDataUrl).then((data) => {
-      setPopulationData(data);
+    json(dataUrl).then((data) => {
+      setData(data);
     });
   }, []);
 
-  console.log(populationData);
-
-  return populationData;
+  return data;
 };
