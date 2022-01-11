85fec0d182b8d9ed6a87be1d4b37a3ff7d956881
Grigoleit Lennart <lennart.grigoleit@tum.de>
2022-01-11 10:14:22 +0100

    hovering works

--- src/components/Choropleth/Marks.js
+++ src/components/Choropleth/Marks.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import { geoPath, geoMercator } from 'd3';
 import Legend from '../../charttools/useLegend';
 
 const missingDataColor = 'darkgray';
 
 const Marks = ({ mapData, width, height, rowByCountry, colorScale, colorValue, legendTitle }) => {
+  const [mouseHover, setMouseHover] = useState(false);
+
+  const handleMouseEnter = (e, d) => {
+    setMouseHover(!mouseHover);
+    console.log(d);
+  };
+
+  const handleMouseLeave = (d) => {
+    setMouseHover(!mouseHover);
+  };
+
   // generate map progjection and paths
   const projection = geoMercator()
     .scale(500)
@@ -19,15 +31,17 @@ const Marks = ({ mapData, width, height, rowByCountry, colorScale, colorValue, l
       <g className="marks">
         {mapData.features.map((feature) => {
           const d = rowByCountry.get(feature.properties.geounit);
-          if (!d) {
-            console.log("Name doesn't match: " + feature.properties.geounit);
-          }
+          // if (!d) {
+          //   console.log("Name doesn't match: " + feature.properties.geounit);
+          // }
 
           return (
             <path
               fill={d ? colorScale(colorValue(d)) : missingDataColor}
-              className="land"
+              className={mouseHover ? 'highlighted' : 'land'}
               d={path(feature)}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
             />
           );
         })}
