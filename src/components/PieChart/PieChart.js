fa33a2876468e24914ea804f9f3d5c44f340b5d3
Grigoleit Lennart <lennart.grigoleit@tum.de>
2022-01-29 20:22:55 +0100

    first PieChart SVG working

--- src/components/PieChart/PieChart.js
+++ src/components/PieChart/PieChart.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { useStoreState } from 'easy-peasy';
-import usePieChart from '../../charttools/usePieChart';
+import PieChartGen from '../../charttools/usePieChart';
 
 const width = 400;
-const height = 400;
+const height = 500;
 
 const PieChart = () => {
   const categoryData = useStoreState((state) => state.categoryData);
-  console.log(categoryData);
   const selectedYear = useStoreState((state) => state.selectedYear);
 
   if (!categoryData) {
@@ -16,11 +15,24 @@ const PieChart = () => {
 
   // filter data for selected year
   const filteredData = categoryData.filter((d) => d.year === selectedYear);
-  console.log(filteredData);
 
-  // const pieChart = usePieChart(filteredData, { title: legendTitle });
+  const pieChart = PieChartGen(filteredData, {
+    name: (d) => d.country,
+    value: (d) => d.patents,
+    width,
+    height: height,
+  });
 
-  return <svg width={width} height={height}></svg>;
+  console.log(pieChart);
+
+  return (
+    <svg width={width} height={height}>
+      <g
+        transform={`translate(${width * 0.5},${height * 0.5})`}
+        dangerouslySetInnerHTML={{ __html: pieChart.innerHTML }}
+      ></g>
+    </svg>
+  );
 };
 
 export default PieChart;
