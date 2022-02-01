6af7c6dba8951742fe11d4eef0e08e01b8b749e6
Grigoleit Lennart <lennart.grigoleit@tum.de>
2022-02-01 21:07:36 +0100

    clean up data pipeline

--- src/datatools/useData.js
+++ src/datatools/useData.js
@@ -27,116 +27,46 @@ export const useData = () => {
   );
   const filteredFlagData = flagData.filter((o1) => countryData.some((o2) => o1.name === o2.name));
 
-  // add years to data objects
+  // add years to data object
   const data = {};
   for (let i = 1996; i <= 2014; i++) {
     data[i] = [];
   }
   console.log(data);
 
+  // loop through every year in data object
   for (let year in data) {
     console.log();
     countryData.forEach((d) => {
       data[year].push({ country: d.name, selected: true });
     });
+    // loop through every array for each year
     data[year].forEach((object) => {
+      // add flags to data
       filteredFlagData.forEach((flagEntry) => {
         if (object.country === flagEntry.name) {
           object.flag = flagEntry.flag;
         }
       });
+      // add patents to data
       filteredPatentData.forEach((patentEntry) => {
         if (object.country === patentEntry.country && Number(year) === patentEntry.year) {
           object.patents = patentEntry.patents;
         }
       });
-      populationData.forEach((populationEntry) => {
+      // add population to data
+      filteredPopulationData.forEach((populationEntry) => {
         if (object.country === populationEntry.country && Number(year) === populationEntry.year) {
           object.population = populationEntry.population;
         }
       });
-      investmentData.forEach((investmentEntry) => {
+      // add investment to data
+      filteredInvestmentData.forEach((investmentEntry) => {
         if (object.country === investmentEntry.country && Number(year) === investmentEntry.year) {
           object.investment = Number(investmentEntry.value.replace(',', '.'));
         }
       });
     });
   }
-
-  // // add countries to data object
-  // for (let key in data) {
-  //   countryData.forEach((d) => {
-  //     data[key][d.name] = { selected: true };
-  //   });
-  //   filteredFlagData.forEach((d) => {
-  //     if (data[key][d.name]) {
-  //       data[key][d.name].flag = d.flag;
-  //     }
-  //   });
-  // }
-
-  // // add number of patents to data object
-  // filteredPatentData.forEach((d) => {
-  //   if (data[d.year][d.country]) {
-  //     data[d.year][d.country].patents = d.patents;
-  //   }
-  // });
-
-  // // add population to data object
-  // filteredPopulationData.forEach((d) => {
-  //   if (data[d.year][d.country]) {
-  //     data[d.year][d.country].population = d.population;
-  //   } else {
-  //     data[d.year][d.country].population = 'no data';
-  //   }
-  // });
-
-  // // add investments to data object
-  // filteredInvestmentData.forEach((d) => {
-  //   if (data[d.year][d.country]) {
-  //     data[d.year][d.country].investment = Number(d.value.replace(',', '.'));
-  //   }
-  // });
-
-  console.log(data);
-  // ###################### OLD ###################
-  // append population to general patentData object
-  patentData.forEach((patentEntry) => {
-    populationData.forEach((populationEntry) => {
-      if (
-        patentEntry.country === populationEntry.country &&
-        patentEntry.year === populationEntry.year
-      ) {
-        patentEntry.population = populationEntry.population;
-      }
-    });
-  });
-
-  // append investmentData to general patentData object
-  patentData.forEach((patentEntry) => {
-    investmentData.forEach((investmentEntry) => {
-      if (
-        patentEntry.country === investmentEntry.country &&
-        patentEntry.year === investmentEntry.year
-      ) {
-        patentEntry.investment = Number(investmentEntry.value.replace(',', '.'));
-      }
-    });
-  });
-
-  // append flagData to general patentData object
-  patentData.forEach((patentEntry) => {
-    flagData.forEach((flagEntry) => {
-      if (patentEntry.country === flagEntry.name) {
-        patentEntry.flag = flagEntry.flag;
-      }
-    });
-  });
-
-  patentData.forEach((patentEntry) => {
-    patentEntry.selected = true;
-  });
-
-  const filteredData = patentData.filter((o1) => countryData.some((o2) => o1.country === o2.name));
-  return filteredData;
+  return data;
 };
